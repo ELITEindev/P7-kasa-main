f05d79ed3e3e684397f749cf08c407d9f1ee5196
ELITEindev <44844449+ELITEindev@users.noreply.github.com>
2026-01-28 11:29:26 -0400

    fix: modif session

--- src/components/Accommodation/PictureCounter.jsx
+++ src/components/Accommodation/PictureCounter.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 function PictureCounter({ pictures, pictureIndex }) {
+    // Ne pas afficher le compteur s'il n'y a qu'une seule image
+    if (pictures.length <= 1) {
+        return null;
+    }
+    
     return (
         <div className='picture_counter'>
             {pictureIndex + 1}/{pictures.length}
